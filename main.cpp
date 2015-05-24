43fce571a9a481cc4a6474496a4cc179f73f8d3c
hsanmort <hassan.akrout@gmail.com>
2015-05-24 16:56:17 +0200

    change

--- main.cpp
+++ main.cpp
@@ -9,11 +9,11 @@ int main()
 {
     //intialization Du jeux
     Pioche p;
-int    n=4;
+    int    n=4;
     while(n>0)
     {
-           p= Creation_pioche(p);
-n--;
+        p= Creation_pioche(p);
+        n--;
     }
     Afficher( p);
 
