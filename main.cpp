82809545a1796a85989af641b6725d9dcc298345
giliwawa <alaa.ksontini@esprit.tn>
2015-05-24 20:16:13 +0200

    window shows up

--- main.cpp
+++ main.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 #include<stdlib.h>
 #include<stdio.h>
-#include "Solitaire.h"
 #include "main.h"
+#include "Solitaire.h"
+
 using namespace std;
 
-int main( int argc, char *argv[] )
+int main( int argc, char *argv[])
 {
-    init("Hsan");
-    //intialization Du jeux
+
     Pioche p;
     Tpioche t;
     int    n=4;
     while(n>0)
     {
-       Creation_pioche(&p);
+        Creation_pioche(&p);
         n--;
     }
+    init("Hsan");
     t=FirstDist( &p,  t);
     AfficherTableau(t);
-    //change
     cout<<"changement de la pile le reste"<<endl;
     p= ChangementDetat( p);
 
     Afficher( p);
-
+    atexit(cleanup);
+    while (1)
+    {
+        /*getInput();
+        draw();
+        delay(frameLimit);
+        frameLimit = SDL_GetTicks() + 16;*/
+    }
+    exit(0);
 }
